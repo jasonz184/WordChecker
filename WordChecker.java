bd73b95cfae19d9912f24cd6b973c6a29b07eb9d
jasonz184 <jasonz184@nycstudents.net>
2024-10-09 15:48:24 +0000

    Wrote method definition

--- WordChecker.java
+++ WordChecker.java
@@ -20,16 +20,17 @@ public class WordChecker {
      * Postcondition: wordList is unchanged.
      */
     public boolean isWordChain() {
-        boolean wordChain = true; 
-        for (int i = 1; i < wordList.size(); i++) { 
-            String after = wordList.get(i); 
-            String before = wordList.get(i - 1); 
-            if(after.indexOf(before) < 0) {
+        boolean wordChain = true;
+        for (int i = 1; i < wordList.size(); i++) {
+            String after = wordList.get(i);
+            String before = wordList.get(i - 1);
+            if (after.indexOf(before) < 0) {
                 wordChain = false;
-            } 
+            }
         }
         return wordChain;
     }
+
     /**
      * Returns an ArrayList<String> based on strings from wordList that start
      * with target, as described in part (b). Each element of the returned ArrayList
